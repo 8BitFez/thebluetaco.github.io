9f4e5e444878076deeb6d76ff71fdbb59523d91f
Saul Villasenor <svillasenor@chicagotechacademy.org>
2015-12-15 17:16:38 +0000

    added nav bar and hover animation to images link

--- js/scripts.js
+++
@@ -1,2 +0,0 @@
-TweenMax.staggerFromTo($('#lightBlueNumbers path'),3,{alpha:1},{alpha:0,repeat:-1,y:"+=30"},0.3)
-TweenMax.staggerFromTo($('#darkBlueNumbers path'),3,{alpha:1},{alpha:0,repeat:-1},0.2)
